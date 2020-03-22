543ec201cba3a405bad471efd181309b62c9abd6
Daniel Rodrigo Fleck <fleck@pm.pr.gov.br>
2020-03-22 16:20:12 -0300

    Implementa mock da api

---
+++ src/Controller/RespostasController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class RespostasController extends AbstractController
+{
+    /**
+     * @Route("/saude/respostas", name="respostas", methods={"GET","POST"})
+     */
+    public function index(Request $request)
+    {
+        $error = array();
+
+        if ($request->isMethod('GET')) {
+            $error[] = 'POST é o único método permitido.';
+        }
+
+        if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+            $data = json_decode($request->getContent(), true);
+            $request->request->replace(is_array($data) ? $data : array());
+        } else {
+            $error[] = 'Cabeçalho deve ter "Content-Type=application/json"';
+        }
+
+
+        return $this->json([
+            'result' => 'Recebido',
+            'erros' => $error,
+            'dados_recebidos' => $data
+        ]);
+    }
+}
